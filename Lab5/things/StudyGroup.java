66b202e25a1be932a9f1459de6b5b001a5ab668d
Gregorgii <64372483+Gregorgii@users.noreply.github.com>
2023-03-13 14:13:37 +0300

    regular edit

--- Lab5/things/StudyGroup.java
+++ Lab5/things/StudyGroup.java
@@ -1,5 +1,9 @@
 package things;
 
+import java.time.LocalDate;
+
+import javax.swing.text.TabExpander;
+
 public class StudyGroup {
     private Integer id; //Поле не может быть null, Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
     private String name; //Поле не может быть null, Строка не может быть пустой
@@ -10,4 +14,54 @@ public class StudyGroup {
     private Integer transferredStudents; //Значение поля должно быть больше 0, Поле может быть null
     private Semester semesterEnum; //Поле не может быть null
     private Person groupAdmin; //Поле не может быть null
+
+    public StudyGroup(Integer id, String name, Coordinates coordinates, java.time.LocalDate creationDate, Long studentsCount, Integer shouldBeExpelled, Integer transferredStudents, Semester semesterEnum, Person groupAdmin){
+        this.id = id;
+        this.name = name;
+        this.coordinates = coordinates;
+        this.creationDate = creationDate;
+        this.studentsCount = studentsCount;
+        this.shouldBeExpelled = shouldBeExpelled;
+        this.transferredStudents = transferredStudents;
+        this.semesterEnum = semesterEnum;
+        this.groupAdmin = groupAdmin;
+    }
+
+    public Integer getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public Coordinates getCoordinates(){
+        return coordinates;
+    }
+
+    public LocalDate getCreationDate(){
+        return creationDate;
+    }
+
+    public Long getStudentsCount(){
+        return studentsCount;
+    }
+
+    public Integer getShouldBeExpelled(){
+        return shouldBeExpelled;
+    }
+
+    public Integer getTransferredStudents(){
+        return transferredStudents;
+    }
+
+    public Semester getSemesterEnum(){
+        return semesterEnum;
+    }
+
+    public Person getGroupAdmin(){
+        return groupAdmin;
+    }
+
+    
 }

6eba46f59add3eb630c2241ee43edb66f444c1af
donRumata03 <donrumatao3@gmail.com>
2023-03-17 14:34:46 +0300

    started on thinkpad

--- Lab5/commands/InfoCommand.java
+++ Lab5/commands/InfoCommand.java
@@ -1,7 +1,22 @@
 package commands;
 
 public class InfoCommand extends AbstractCommand {
-    public InfoCommand(){
-        
+    private final CollectionManager collectionManager;
+
+    public InfoCommand(CollectionManager collectionManager) {
+        super("info", "print information about the collection to the standard output stream (type, initialization date, number of elements, etc.)");
+        this.collectionManager = collectionManager;
+    }
+
+    @Override
+    public boolean execute(String argument) {
+        try{
+            if(!argument.isEmpty()) throw new WrongAmountOfElementsException();
+            Console.printLn(collectionManager.infoAboutCollection());
+            return true;
+        } catch (WrongAmountOfElementsException e){
+            Console.printError("Usage of (" + argument + ") in " + getName());
+        }
+        return false;
     }
 }
